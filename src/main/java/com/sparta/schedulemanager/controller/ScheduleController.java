9967a0788be598087d5a286a865910463861db05
pkhyun <pkh954533@gmail.com>
2024-05-15 18:11:45 +0900

    개인과제 5단계 완료

--- src/main/java/com/sparta/schedulemanager/controller/ScheduleController.java
+++ src/main/java/com/sparta/schedulemanager/controller/ScheduleController.java
@@ -1,12 +1,46 @@
 package com.sparta.schedulemanager.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.schedulemanager.dto.ScheduleRequestDto;
+import com.sparta.schedulemanager.dto.ScheduleResponseDto;
+import com.sparta.schedulemanager.service.ScheduleService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ScheduleController {
 
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping("/schedule")
+    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto) {
+        return scheduleService.createSchedule(requestDto);
+    }
+
+    @GetMapping("/schedule")
+    public ScheduleResponseDto getSchedule(@RequestParam int id) {
+        return scheduleService.getSchedule(id);
+    }
+
+    @GetMapping("/schedules")
+    public List<ScheduleResponseDto> getSchedules() {
+        return scheduleService.getSchedules();
+    }
+
+    @PutMapping("/schedule/{id}")
+    public ScheduleResponseDto updateSchedule(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
+        return scheduleService.updateSchedule(id, requestDto);
+    }
+
+    @DeleteMapping("/schedule/{id}")
+    public void deleteSchedule(@PathVariable int id, @RequestBody ScheduleRequestDto requestDto) {
+        scheduleService.deleteSchedule(id, requestDto);
+    }
 
 
 }
--- src/main/java/com/sparta/schedulemanager/service/ScheduleService.java
+++ src/main/java/com/sparta/schedulemanager/service/ScheduleService.java
@@ -1,7 +1,63 @@
 package com.sparta.schedulemanager.service;
 
+import com.sparta.schedulemanager.dto.ScheduleRequestDto;
+import com.sparta.schedulemanager.dto.ScheduleResponseDto;
+import com.sparta.schedulemanager.entity.Schedule;
+import com.sparta.schedulemanager.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public ScheduleResponseDto createSchedule(ScheduleRequestDto requestDto) {
+        Schedule schedule = new Schedule(requestDto);
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new ScheduleResponseDto(schedule);
+    }
+
+    public ScheduleResponseDto getSchedule(int id) { // 선택 일정 조회
+        Schedule schedule = findScheduleById(id);
+        return new ScheduleResponseDto(schedule);
+    }
+
+    public List<ScheduleResponseDto> getSchedules() { // 전체 일정 조회
+        return scheduleRepository.findAll().stream().map(ScheduleResponseDto::new).toList();
+    }
+
+    public ScheduleResponseDto updateSchedule(int id, ScheduleRequestDto requestDto) { //선택 일정 수정
+        Schedule schedule = findScheduleById(id);
+        if (checkPassword(schedule, requestDto)) {
+            schedule.update(requestDto);
+            return new ScheduleResponseDto(schedule); // 수정된 일정의 정보를 반환 받아 확인
+        } else {
+            throw new IllegalArgumentException("패스워드가 일치하지 않습니다.");
+        }
+    }
+
+    public void deleteSchedule(int id, ScheduleRequestDto requestDto) { // 선택 일정 삭제
+        Schedule schedule = findScheduleById(id);
+        if (checkPassword(schedule, requestDto)) {
+            scheduleRepository.delete(schedule);
+        } else {
+            throw new IllegalArgumentException("패스워드가 일치하지 않습니다.");
+        }
+    }
+
+    private Schedule findScheduleById(int id) { // id 존재 확인 메서드
+        return scheduleRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 스케줄을 찾을 수 없습니다."));
+    }
+
+    private boolean checkPassword(Schedule schedule, ScheduleRequestDto requestDto) { // password 일치 여부 확인
+        return schedule.getPassword().equals(requestDto.getPassword());
+    }
+
 }
